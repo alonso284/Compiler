e94686a2fe7ad77e6fd771a1f6332caf8d7686b7
Alepmzz <A01722539@tec.mx>
2026-05-14 13:32:31 -0600

    statements

--- nodes_ast.py
+++ nodes_ast.py
@@ -22,3 +22,93 @@ class ProcedureNode:
 @dataclass
 class BlockNode:
     statements: List[Any]
+
+
+@dataclass
+class AssignmentNode:
+    variable: Any
+    expression: Any
+
+
+@dataclass
+class WritelnNode:
+    expression: Any
+
+
+@dataclass
+class IncrementNode:
+    name: str
+
+
+@dataclass
+class DecrementNode:
+    name: str
+
+
+@dataclass
+class WhileNode:
+    condition: Any
+    body: List[Any]
+
+
+@dataclass
+class ForNode:
+    init: Any
+    condition: Any
+    update: Any
+    body: List[Any]
+
+
+@dataclass
+class IfNode:
+    condition: Any
+    then_body: List[Any]
+    else_body: Optional[List[Any]] = None
+
+
+# ==========================
+# EXPRESSIONS
+# ==========================
+
+@dataclass
+class BinaryOpNode:
+    left: Any
+    operator: str
+    right: Any
+
+
+@dataclass
+class UnaryOpNode:
+    operator: str
+    value: Any
+
+
+@dataclass
+class VariableNode:
+    name: str
+    index: Optional[Any] = None
+
+
+@dataclass
+class IntegerNode:
+    value: int
+
+
+@dataclass
+class FloatNode:
+    value: float
+
+
+@dataclass
+class StringNode:
+    value: str
+
+
+@dataclass
+class CharNode:
+    value: str
+
+
+@dataclass
+class BoolNode:
+    value: bool
